8606933c466601a2c3720819690b671b25216a62
grant-murphy <murphyg.gm@gmail.com>
2019-06-25 01:21:29 +0000

    assertEqualRevised

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log('❌❌❌: ' + actual + ' !== ' + expected);
+  if (actual === expected) {
+    console.log(`✅✅✅: ${actual} === ${expected}`);
   } else {
-    console.log('✅✅✅: ' + actual + ' === ' + expected);
+    console.log(`❌❌❌: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1); 
+assertEqual(1, 1);
