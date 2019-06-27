b4551cd65437d04f9891e89cb0d29fc7a75d6ca9
grant-murphy <murphyg.gm@gmail.com>
2019-06-27 01:24:13 +0000

    middleRevised

--- middle.js
+++ middle.js
@@ -1,3 +1,15 @@
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+}
+
+
 const middle = function(arr) {
   let result = [];
   if (arr.length === 1 || arr.length === 2) {
@@ -13,10 +25,10 @@ const middle = function(arr) {
 }
 
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+console.log(eqArrays(middle([1, 2, 3, 4, 5, 6]);
+// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
 
