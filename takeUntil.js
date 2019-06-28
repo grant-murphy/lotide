c3bddd1caa3ede7d7d3311b63afa070c4b6743d9
grant-murphy <murphyg.gm@gmail.com>
2019-06-28 00:14:36 +0000

    takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,24 @@
+const takeUntil = function(array, callback) {
+const numbers = [];
+for(let num of array) {
+  if(callback(num)) {
+    break;
+  }
+  numbers.push(num)
+  } 
+  return numbers;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+// console.log(takeUntil([1,2,-3,4, -5, 5], x => x < 0))
