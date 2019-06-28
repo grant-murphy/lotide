dd64b0a75efae2ee1e2b63e2d4f3ad2ce6ab3329
grant-murphy <murphyg.gm@gmail.com>
2019-06-28 00:13:36 +0000

    mapNeedsAssertEqual

--- map.js
+++ map.js
@@ -1,11 +1,16 @@
-// // FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌❌❌: ${actual} !== ${expected}`);
-//   }
-// };
+//NOT FINISHED
+
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+}
+
 
 
 const words = ["ground", "control", "to", "major", "tom"];
@@ -21,6 +26,6 @@ const map = function(array, callback) {
 
 const results1 = map(words, word => word[0]);
 
-
+console.log(eqArrays(results1, ['g','1','t','m','t']))
 // REVISE TO ASSERT EQUAL
 
