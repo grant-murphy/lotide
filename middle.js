eb5a28eeebf2c8a4841d1ae825019b2acf26b22c
grant-murphy <murphyg.gm@gmail.com>
2019-07-01 21:24:31 +0000

    Refactored with Mocha & Chai

--- middle.js
+++ middle.js
@@ -1,15 +1,3 @@
-let eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-}
-
-
 const middle = function(arr) {
   let result = [];
   if (arr.length === 1 || arr.length === 2) {
@@ -17,18 +5,29 @@ const middle = function(arr) {
   } else if (arr.length % 2 === 1) {
     result.push(arr[Math.floor(arr.length / 2)]);
   } else {
-    result.push(arr[(Math.floor(arr.length / 2)) - 1])
-    result.push(arr[Math.floor(arr.length / 2)])
+    result.push(arr[(Math.floor(arr.length / 2)) - 1]);
+    result.push(arr[Math.floor(arr.length / 2)]);
 
   }
   return result;
-}
+};
 
+module.exports = middle;
 
-console.log(eqArrays(middle([1, 2, 3, 4, 5, 6]);
-// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+//console.log(middle([1, 2]));
 // console.log(middle([1, 2, 3]));
 // console.log(middle([1, 2, 3, 4, 5]));
 // console.log(middle([1, 2, 3, 4]));
 // console.log(middle([1, 2, 3, 4, 5, 6]));
 
+// let eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (var i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   } return true;
+// }
