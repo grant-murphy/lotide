5c2fa2ecf14c283bb372df03f587ddfa6ade0486
grant-murphy <murphyg.gm@gmail.com>
2019-07-01 21:20:34 +0000

    Refactored with Mocha & Chai

--- eqArrays.js
+++ eqArrays.js
@@ -2,17 +2,11 @@ let eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (var i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   } return true;
-}
+};
 
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+module.exports = eqArrays;
