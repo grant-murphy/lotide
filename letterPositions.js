074d7330b0b0b414b9a1a75fc954ec1458b322f3
grant-murphy <murphyg.gm@gmail.com>
2019-07-01 21:31:58 +0000

    Refactored with Mocha & Chai

--- letterPositions.js
+++ letterPositions.js
@@ -2,16 +2,17 @@
 const countLetters = function(letterCount) {
   const results = {};
   
-  for (let i = 0; i < letterCount.length; i++) {   
+  for (let i = 0; i < letterCount.length; i++) {
     
-      if(results[letterCount[i]]) {
-        results[letterCount[i]].push(i);
-      } else {
-        results[letterCount[i]] = [i];
-      }
+    if (results[letterCount[i]]) {
+      results[letterCount[i]].push(i);
+    } else {
+      results[letterCount[i]] = [i];
     }
+  }
   return results;
-}
+};
 
+module.exports = countLetters;
 
-console.log(countLetters("lighthouse in the house"))
+// console.log(countLetters("lighthouse in the house"))
