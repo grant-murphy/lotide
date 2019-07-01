024849297b307af109c2897e9d64a54b24fd7869
grant-murphy <murphyg.gm@gmail.com>
2019-07-01 21:22:22 +0000

    Refactored with Mocha & Chai

--- eqObjects.js
+++ eqObjects.js
@@ -1,33 +1,30 @@
-let eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-}
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
-  if(Object.keys(object1).length !== Object.keys(object2).length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } else {
-    for(let key in object1) {
-      if(!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+    for (let key in object1) {
+      if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+        
         return false;
-      } else if(Array.isArray(object1[key]) && !eqArrays(object1[key], object2[key])) {
-
+        
+      } else if (Array.isArray(object1[key]) && !eqArrays(object1[key], object2[key])) {
+        
         return false;
-      } 
+      }
     }
-  } 
+  }
   return true;
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+module.exports = eqObjects;
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
